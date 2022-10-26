d1c0491f4028f29fcf221154746628483169378d
Giovanna Alves de Farias <101728198+giovanna-alves-de-farias@users.noreply.github.com>
2022-10-26 11:48:55 -0300

    first commit

---
+++ Controllers/ComputerController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using MvcLabManager.Models;
+
+namespace MvcLabManager.Controllers;
+
+public class ComputerController : Controller
+{
+    private LabManagerContext _context;
+
+    public ComputerController(LabManagerContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Index()
+    {
+
+        return View(_context.Computers);
+    }
+
+    public IActionResult Show(int id)
+    {
+        Computer? computer = _context.Computers.Find(id);
+
+        if(computer == null)
+        {
+            return NotFound(); //return RedirectToAction("Index");
+        }
+
+        return View(computer);
+    }
+}
